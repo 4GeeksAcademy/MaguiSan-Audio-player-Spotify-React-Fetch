66a0b9ff6f11a1386f0b833196aa55b33127d57f
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-08-18 09:01:27 +0000

    next and previous

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect, useRef} from "react";
+
 const Home = () => {
     const [songsList, setSongsList] = useState([])
 	const [urlSong, setUrlSong] = useState("")
 	const [isPlaying, setIsPlaying] = useState(false)
-	const playSong = useRef()
-	const pausePlay = useRef()
+	const [songIndex, setSongIndex] = useState (0)
+	const refAudio = useRef()
 
     const getSongsList = () => {
 		fetch("https://playground.4geeks.com/sound/songs")
@@ -13,58 +14,76 @@ const Home = () => {
 	};
 
 	const getUrlSong = (url) => {
-		setUrlSong(url)
-		playSong.current.src= (`https://playground.4geeks.com${urlSong}`)
-		playSong.current.play();
+		console.log("soy la cancion",urlSong);
+		setUrlSong(url);
+		refAudio.current.src= (`https://playground.4geeks.com${urlSong}`)
+		refAudio.current.play();
 		// console.log("me estas dando click", urlSong)
 	}
 
-	const pauseSong = () => {
+	const pausePlay = () => {
 		if (isPlaying) {
-			playSong.current.pause();
+			refAudio.current.pause();
 		} else {
-			playSong.current.play();
+			refAudio.current.play();
 		}
 	}
 
-	// previousSong
+	const previousSong = () => {
+		setSongIndex((prevIndex) => (prevIndex - 1 + songsList.length) % songsList.length);
+		getUrlSong(songsList[songIndex].url);
+	};
+
+	const nextSong = () => {
+		setSongIndex((prevIndex) => (prevIndex + 1) % songsList.length);
+		getUrlSong(songsList[songIndex].url);
+	};
 
     useEffect(()=>{
         getSongsList()
-		if (playSong.current) {
-			playSong.current.addEventListener("play", () => {
+		if (refAudio.current) {
+			refAudio.current.addEventListener("play", () => {
 				setIsPlaying(true);
 			})
-			playSong.current.addEventListener("pause", () => {
+			refAudio.current.addEventListener("pause", () => {
 				setIsPlaying(false);
 			})
 			return () => {
-				playSong.current.removeEventListener("play", () => {
+				refAudio.current.removeEventListener("play", () => {
 					setIsPlaying(true)
 				})
-				playSong.current.removeEventListener("pause", () => {
+				refAudio.current.removeEventListener("pause", () => {
 					setIsPlaying(false);
 				})
 			} 
 		}
     }, [])
 	
-	// boton la sgte cancion
-	//efect sound
-	
 	return (
-		<div className="text-center">
-            <label>Lista de canciones</label>
-			{
-				songsList.map((item,index)=>(
-					<h5 key = {index} onClick = {() => getUrlSong(item.url)}>{item.name}</h5>
-					// al dar click obtendre la url
-				))
-			}
-			<audio src="" ref={playSong}></audio>
-			<button >PREVIOUS</button>
-			<button onClick = {pauseSong} ref={pausePlay}>{isPlaying ? 'play' : 'pause'}</button>
-			<button >NEXT</button>
+		<div className="container-fluid p-0 w-100 bg-dark">
+            <h3 className="bg-dark text-secondary text-center p-2">Songs List</h3>
+			<ol className="list-group mh-100 list-group-flush">
+				{
+					songsList.map((item, index) => (
+						<li className="list-group-item list-group-item-action list-group-item-dark text-black fs-4" key={index} onClick={() => getUrlSong(item.url)}><span className="me-4">{item.id}</span> {item.name}</li>
+					))
+				}
+				<li></li>
+			</ol>
+			<audio src="" ref={refAudio}></audio>
+			<div className="fixed-bottom bg-dark text-center p-2">
+				<div className="row g-1">
+					<div className="col-4 text-end pt-2">
+						<button className="rounded border-0 px-2 m-2" onClick={previousSong}><i className="fa-solid fa-caret-left fa-lg"></i></button>
+					</div>
+					<div className="col-4">
+						<button className="rounded border-0 p-2 m-2" onClick={pausePlay}>{isPlaying ? <i className="fa-solid fa-pause fa-xl px-1"></i> : <i className="fa-solid fa-play fa-2xl"></i>}</button>
+					</div>
+					<div className="col-4 text-start pt-2">
+						<button className="rounded border-0 px-2 m-2" onClick={nextSong}><i className="fa-solid fa-caret-right fa-lg"></i></button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
